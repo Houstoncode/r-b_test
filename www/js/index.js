fd0786321a584d3941aec96a998bdba9f929ccd5
Sergey Pavlov <houston.inc@yandex.ru>
2021-01-18 11:19:01 +0300

    fix: add files

--- www/js/index.js
+++ www/js/index.js
@@ -21,9 +21,48 @@
 // See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
 document.addEventListener('deviceready', onDeviceReady, false);
 
+    function receivedEvent() {
+  //START ONESIGNAL CODE
+  //Remove this method to stop OneSignal Debugging
+  window.plugins.OneSignal.setLogLevel({logLevel: 6, visualLevel: 0});
+
+  console.log("received One Signal")
+
+  var notificationOpenedCallback = function(jsonData) {
+    console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
+  };
+  // Set your iOS Settings
+  var iosSettings = {};
+  iosSettings["kOSSettingsKeyAutoPrompt"] = false;
+  iosSettings["kOSSettingsKeyInAppLaunchURL"] = false;
+
+  window.plugins.OneSignal
+    .startInit("056bc2a3-a8fc-4cf1-83ad-96a388a9b53d")
+    .handleNotificationOpened(notificationOpenedCallback)
+    .iOSSettings(iosSettings)
+    .inFocusDisplaying(window.plugins.OneSignal.OSInFocusDisplayOption.Notification)
+    .endInit();
+
+  // The promptForPushNotificationsWithUserResponse function will show the iOS push notification prompt. We recommend removing the following code and instead using an In-App Message to prompt for notification permission (See step 6)
+  window.plugins.OneSignal.promptForPushNotificationsWithUserResponse(function(accepted) {
+    console.log("User accepted notifications: " + accepted);
+  });
+  //END ONESIGNAL CODE
+} 
+
 function onDeviceReady() {
     // Cordova is now initialized. Have fun!
 
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
+
+    var ref = cordova.InAppBrowser.open('http://site.com', "_blank");
+    ref.addEventListener("loadstop", loadStopCallBack);
+
     document.getElementById('deviceready').classList.add('ready');
+
+    receivedEvent();
+}
+
+function loadStopCallBack() {
+    navigator.splashscreen.hide();
 }
