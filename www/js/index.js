81acbd06e8bcfcc35dd368ad3ce3042e70891d6b
Sergey Pavlov <houston.inc@yandex.ru>
2021-01-19 10:20:33 +0300

    fix: requested changes

--- www/js/index.js
+++ www/js/index.js
@@ -1,37 +1,8 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-
-// Wait for the deviceready event before using any of Cordova's device APIs.
-// See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
 document.addEventListener('deviceready', onDeviceReady, false);
 
-  function receivedEvent() {
-    //START ONESIGNAL CODE
-    //Remove this method to stop OneSignal Debugging
-    window.plugins.OneSignal.setLogLevel({logLevel: 6, visualLevel: 0});
+var refBrowser;
 
-    console.log("received One Signal")
-
-    var notificationOpenedCallback = function(jsonData) {
-      console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
-    };
-    // Set your iOS Settings
+function receivedEvent() {
     var iosSettings = {};
     iosSettings["kOSSettingsKeyAutoPrompt"] = false;
     iosSettings["kOSSettingsKeyInAppLaunchURL"] = false;
@@ -42,36 +13,24 @@ document.addEventListener('deviceready', onDeviceReady, false);
       .iOSSettings(iosSettings)
       .inFocusDisplaying(window.plugins.OneSignal.OSInFocusDisplayOption.Notification)
       .endInit();
-
-    // The promptForPushNotificationsWithUserResponse function will show the iOS push notification prompt. We recommend removing the following code and instead using an In-App Message to prompt for notification permission (See step 6)
-    window.plugins.OneSignal.promptForPushNotificationsWithUserResponse(function(accepted) {
-      console.log("User accepted notifications: " + accepted);
-    });
-    //END ONESIGNAL CODE
 } 
 
 function onDeviceReady() {
-    // Cordova is now initialized. Have fun!
-
-    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-
-    var ref = cordova.InAppBrowser.open('http://site.com', "_blank");
-    ref.addEventListener("loadstart", loadStartCallBack);
-    ref.addEventListener("loadstop", loadStopCallBack);
-
-    document.getElementById('deviceready').classList.add('ready');
-
-    receivedEvent();
-}
-
-function loadStartCallBack() {
   navigator.splashscreen.show();
 
-  setTimeout(() => {
-    navigator.splashscreen.hide();
-  }, 5000)
+  setTimeout(function() {
+      refBrowser.show();
+      navigator.splashscreen.hide();
+  }, 5000);
+
+  refBrowser = cordova.InAppBrowser.open('http://site.com', "_blank", 'hideurlbar=yes,hidenavigationbuttons=yes,toolbar=no,location=no,hidden=yes,zoom=no');
+  refBrowser.addEventListener("loadstop", loadStopCallBack);
+
+  receivedEvent();
 }
 
+
 function loadStopCallBack() {
-    navigator.splashscreen.hide();
+  refBrowser.show();
+  navigator.splashscreen.hide();
 }
